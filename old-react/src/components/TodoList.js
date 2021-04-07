f12e54db0e506fab990da397a1d9b77d030cf0d2
Aaron Weissman <weissman.aaron@gmail.com>
2021-04-06 20:47:06 -0600

    Animate the old react app

--- old-react/src/components/TodoList.js
+++ old-react/src/components/TodoList.js
@@ -3,21 +3,34 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 
 import { removeTodo, setTodos, DB_URL } from '../redux/actions';
+import { animateButton } from '../utils/animate';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.btnRef = React.createRef();
+  }
+
   componentDidMount() {
+    // Fetch all the todos from the db
     this.props.setTodos();
+    // add a confetti event listener
+    this.btnRef.current.addEventListener('click', animateButton, false);
+  }
+
+  componentWillUnmount() {
+    this.btnRef.current.removeEventListener('click', animateButton);
   }
 
   removeTodo = async todo_id => {
     await axios.delete(`${DB_URL}/${todo_id}`);
-
     this.props.removeTodo(todo_id);
   };
 
   render() {
     return (
-      <ul className="todo-list">
+      <ul className="todo-list confetti" ref={this.btnRef}>
         {this.props.todos.map(({ title, id }) => (
           <li className="todo" key={id}>
             <h3>{title}</h3>
---
+++ old-react/src/utils/animate.js
@@ -0,0 +1,10 @@
+export const animateButton = function (e) {
+  e.preventDefault();
+  //reset animation
+  e.target.classList.remove('animate');
+
+  e.target.classList.add('animate');
+  setTimeout(function () {
+    e.target.classList.remove('animate');
+  }, 700);
+};
